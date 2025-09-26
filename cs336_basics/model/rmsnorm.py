6b1345abbfeab842e2f2f3ce9dcec85915934e00
Iceberg <gzs_iceberg@outlook.com>
2025-09-26 16:19:11 +0800

    Add RMSNorm class and integrate into run_rmsnorm function for normalization

---
+++ cs336_basics/model/rmsnorm.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+from einops import rearrange, einsum
+import numpy as np
+
+
+class RMSNorm(nn.Module):
+    def __init__(
+        self, dim: int, eps: float = 1e-5, device: torch.device | None = None, dtype: torch.dtype | None = None
+    ):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.scale = nn.Parameter(torch.ones(dim, device=device, dtype=dtype))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.shape[-1] == self.dim, f"Input dimension {x.shape[-1]} does not match RMSNorm dimension {self.dim}"
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        sqr_x = x.pow(2).sum(dim=-1, keepdim=True) / self.dim + self.eps
+        rms_x = torch.sqrt(sqr_x)
+        x = x / rms_x
+        x = x * self.scale
+        return x.to(in_dtype)
+
+    def extra_repr(self) -> str:
+        return f"dim={self.dim}, eps={self.eps}"
